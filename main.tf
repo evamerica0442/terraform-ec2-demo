16c4a3408547c994b9dad42037af5f99b59c6f67
evamerica0442 <104032355+evamerica0442@users.noreply.github.com>
2026-05-31 20:10:07 +0000

    Add Terraform files only

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-00e801948462f718a" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Atlantis-Demo-Instance"
+  }
+}
